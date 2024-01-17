1cf5297ee05b1779c4e10408a487da2bf5a4ff5b
Rio <93978178+Riotic@users.noreply.github.com>
2024-01-17 15:09:05 +0100

    Delete BddCompare directory

--- BddCompare/config/security.config.js
+++
@@ -1,8 +0,0 @@
-// works with passport as a middleware, redirect user to index page if he is not connected
-exports.ensureAuthenticated = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.status(403).redirect('/');
-    }
-  }
--- BddCompare/routers/users/user.routes.js
+++
@@ -1,7 +0,0 @@
-const { userNew, userCreate } = require('../../controllers/user.controller');
-const router = require('express').Router();
-
-// router.get('/new', userNew);
-// router.post('/', userCreate);
-
-module.exports = router;
